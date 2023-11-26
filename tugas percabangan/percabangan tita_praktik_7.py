34fdfd7d5178132715ca5be3723810ea258f751d
TitaJulia <148430832+TitaJulia@users.noreply.github.com>
2023-11-26 08:32:20 +0700

    Add files via upload

---
+++ tugas percabangan/percabangan tita_praktik_7.py
@@ -0,0 +1,12 @@
+#Nama           :Tita Julia Agatha
+#Kelas          :XI-TKJ-1
+#Nomor Absen    :25
+
+# Input informasi pembaruan perangkat lunak
+pembaruan_perlukan_restart = input("Apakah pembaruan perangkat lunak memerlukan restart? (ya/tidak): ")
+
+# Memeriksa apakah sistem perlu di-restart
+if pembaruan_perlukan_restart.lower() == "ya":
+    print("Sistem perlu di-restart.")
+else:
+    print("Sistem tidak perlu di-restart.")
